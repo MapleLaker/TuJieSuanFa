69ffaef2754516af5d9f86dd91748ff433549e21
MapleLaker <31602441+MapleLaker@users.noreply.github.com>
2018-04-18 22:59:16 +0800

    Create binary search.py

---
+++ binary search.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def search_binary( mylist, item):
+	low = 0
+	high = len(mylist)-1
+
+	while 1:
+		mid = (low + high)/2
+		guess = list1[mid]
+
+		if guess == item:
+			return mid
+		elif guess > item:
+			high = mid
+		elif guess < item:
+			low = mid
+
+	return mid
+
+list1 = [1,2,3,4,5]
+a = search_binary(list1, 1)
+
+print a 
